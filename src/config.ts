67bb4219dbd682801bae365997aa0906134b1ef4
Benjamin Hutchins <ben@hutchins.co>
2020-02-19 19:56:34 -0500

    feat: allow any DynamoDB configuration option to be defined

--- src/config.ts
+++ src/config.ts
@@ -7,6 +7,9 @@ export default class Config {
     if (!this.__defaultConnection) {
       this.__defaultConnection = new DynamoDBConnection({
         endpoint: process.env.DYNAMO_ENDPOINT,
+        region: process.env.DYNAMO_REGION || process.env.AWS_REGION,
+        accessKeyId: process.env.DYNAMO_ACCESS_KEY_ID || process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.DYNAMO_SECRET_ACCESS_KEY || process.env.AWS_SECRET_ACCESS_KEY,
         enableAWSXray: process.env.ENABLE_XRAY === 'true',
       })
     }
--- src/connections/dynamodb-connection.ts
+++ src/connections/dynamodb-connection.ts
@@ -3,18 +3,16 @@ import { Agent as HTTPAgent } from 'http'
 import { Agent as HTTPSAgent } from 'https'
 import { Connection } from './connection'
 
+interface DyngooseDynamoDBConnectionOptions extends AWS.DynamoDB.ClientConfiguration {
+  enableAWSXray?: boolean
+}
+
 export class DynamoDBConnection implements Connection {
   private __client: AWS.DynamoDB
 
-  constructor(options: {
-    endpoint?: string,
-    enableAWSXray?: boolean,
-  }) {
-    const dynamoDBOptions = {
-      endpoint: options.endpoint,
-      httpOptions: {
-        agent: this.httpAgent(options.endpoint),
-      },
+  constructor(options: DyngooseDynamoDBConnectionOptions) {
+    options.httpOptions = {
+      agent: this.httpAgent(options.endpoint),
     }
 
     if (options.enableAWSXray) {
@@ -22,9 +20,9 @@ export class DynamoDBConnection implements Connection {
       // importing this only when it's needed
       const AWSXRay = require('aws-xray-sdk-core')
       const aws = AWSXRay.captureAWS(AWS)
-      this.__client = new aws.DynamoDB(dynamoDBOptions)
+      this.__client = new aws.DynamoDB(options)
     } else {
-      this.__client = new AWS.DynamoDB(dynamoDBOptions)
+      this.__client = new AWS.DynamoDB(options)
     }
   }
 
