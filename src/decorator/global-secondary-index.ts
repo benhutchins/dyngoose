bfaa66fbc3270f27e5144ba9f0980dc5e01e8aad
Benjamin Hutchins <ben@hutchins.co>
2020-10-29 14:36:06 -0400

    feat: check the nonKeyAttributes specified to a GSI
    1. throw a SchemaError if an attribute does not exist
    2. remove key attributes automatically
    3. ensure the list is unique, as DynamoDB accepts duplicates in the
       key but then acts oddly about it

--- src/decorator/global-secondary-index.ts
+++ src/decorator/global-secondary-index.ts
@@ -1,3 +1,4 @@
+import { uniq } from 'lodash'
 import { SchemaError } from '../errors'
 import { IThroughput } from '../interfaces'
 import * as Metadata from '../metadata'
@@ -25,6 +26,23 @@ export function GlobalSecondaryIndex(options: GlobalSecondaryIndexOptions) {
       throughput = options.throughput
     }
 
+    if (options.projection === 'INCLUDE') {
+      if (options.nonKeyAttributes == null || options.nonKeyAttributes.length === 0) {
+        throw new SchemaError('If Projection type INCLUDE is specified, some non-key attributes to include in the projection must be specified as well')
+      }
+
+      // 1. verify each attribute specified in nonKeyAttributes exists on the table
+      // 2. remove the key attributes from nonKeyAttributes, as that causes problems
+      // 3. remove duplicates as well
+      options.nonKeyAttributes = uniq(options.nonKeyAttributes.filter((attributeName) => {
+        // throws a SchemaError if the attribute does not exist
+        table.schema.getAttributeByName(attributeName)
+
+        // remove the attribute if it is a key attribute, as it is not necessary to be specified
+        return !(attributeName === options.hashKey || attributeName === options.rangeKey)
+      }))
+    }
+
     const index: Metadata.Index.GlobalSecondaryIndex = {
       propertyName,
       name: options.name == null ? propertyName : options.name,
@@ -33,10 +51,6 @@ export function GlobalSecondaryIndex(options: GlobalSecondaryIndexOptions) {
       projection: options.projection,
       nonKeyAttributes: options.nonKeyAttributes,
       throughput,
-    }
-
-    if (index.projection === 'INCLUDE' && (options.nonKeyAttributes?.length === 0)) {
-      throw new SchemaError('If Projection type INCLUDE is specified, some non-key attributes to include in the projection must be specified as well')
     }
 
     table.schema.globalSecondaryIndexes.push(index)
