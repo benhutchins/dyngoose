8d0962e896f7d6dfe67c480237abb7ffabad57bb
Benjamin Hutchins <ben@hutchins.co>
2020-10-29 14:36:06 -0400

    feat: check the nonKeyAttributes specified to a GSI
    1. throw a SchemaError if an attribute does not exist
    2. remove key attributes automatically
    3. ensure the list is unique, as DynamoDB accepts duplicates in the
       key but then acts oddly about it

--- src/query/global-secondary-index.spec.ts
+++ src/query/global-secondary-index.spec.ts
@@ -1,4 +1,4 @@
-import { expect } from 'chai'
+import { expect, should } from 'chai'
 import * as Decorator from '../decorator'
 import { QueryError } from '../errors'
 import { Table } from '../table'
@@ -18,13 +18,20 @@ class Card extends Table {
   @Decorator.GlobalSecondaryIndex({ hashKey: 'id', rangeKey: 'title' })
   public static readonly filterableTitleIndex: Query.GlobalSecondaryIndex<Card>
 
+  @Decorator.GlobalSecondaryIndex({
+    hashKey: 'id',
+    projection: 'INCLUDE',
+    nonKeyAttributes: ['title'],
+  })
+  public static readonly includeTestIndex: Query.GlobalSecondaryIndex<Card>
+
   @Decorator.Attribute.Number()
   public id: number
 
   @Decorator.Attribute.String()
   public title: string
 
-  @Decorator.Attribute.String()
+  @Decorator.Attribute.Number()
   public count: number
 
   customMethod(): number {
@@ -33,15 +40,15 @@ class Card extends Table {
 }
 
 describe('Query/GlobalSecondaryIndex', () => {
+  beforeEach(async () => {
+    await Card.createTable()
+  })
+
+  afterEach(async () => {
+    await Card.deleteTable()
+  })
+
   describe('hash only index', () => {
-    beforeEach(async () => {
-      await Card.createTable()
-    })
-
-    afterEach(async () => {
-      await Card.deleteTable()
-    })
-
     describe('#query', () => {
       it('should find items', async () => {
         await Card.new({ id: 10, title: 'abc' }).save()
@@ -125,14 +132,6 @@ describe('Query/GlobalSecondaryIndex', () => {
   })
 
   describe('hash and range index', () => {
-    beforeEach(async () => {
-      await Card.createTable()
-    })
-
-    afterEach(async () => {
-      await Card.deleteTable()
-    })
-
     describe('#query', () => {
       it('should find items', async () => {
         await Card.new({ id: 10, title: 'abc' }).save()
@@ -179,6 +178,24 @@ describe('Query/GlobalSecondaryIndex', () => {
         expect(cardIds).to.include.members(res2.records.map((r) => r.id))
         expect(cardIds).to.include.members(res3.records.map((r) => r.id))
       })
+    })
+  })
+
+  describe('include projection index', () => {
+    it('allows you to query and returns the nonKeyAttributes', async () => {
+      const newCard = Card.new({ id: 10, title: 'abc' })
+      newCard.count = 10
+      await newCard.save()
+
+      const card = await Card.includeTestIndex.get({ id: 10 })
+
+      should().exist(card)
+
+      if (card != null) {
+        expect(card.id).to.eq(10)
+        expect(card.title).to.eq('abc')
+        should().not.exist(card.count)
+      }
     })
   })
 })
