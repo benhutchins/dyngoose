a54f172f7d7b11ed01025b6b4631097bc2c058ba
Benjamin Hutchins <ben@hutchins.co>
2020-11-09 21:47:12 -0500

    fix: allow Node v12 and up, need to switch to matrix

--- src/table.ts
+++ src/table.ts
@@ -327,7 +327,7 @@ export class Table {
    *
    * Unlike {@link this.get}, this excepts the attribute name, not the property name.
    */
-  public getAttribute(attributeName: string): DynamoDB.AttributeValue {
+  public getAttribute(attributeName: string): any {
     const attribute = this.table.schema.getAttributeByName(attributeName)
     return this.getByAttribute(attribute)
   }
