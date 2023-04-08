4abf8f4c42727b249f772c74dfef079fe733486a
Benjamin Hutchins <ben@hutchins.co>
2023-04-08 01:48:30 -0400

    feat: add support for List attributes
    Relies on AWS's marshall utilities.
    
    Fixes #608

--- src/decorator/attribute-types/list.ts
+++ src/decorator/attribute-types/list.ts
@@ -1,30 +1,22 @@
-// list: {
-//   name: 'list',
-//   dynamo: 'L',
-//   isSet: true,
-//   dynamofy: JSON.stringify,
-//   dedynamofy: function listify(v: any, attr: Attribute) {
-//     if (!v) {
-//       return
-//     }
+import { type AttributeValue } from '@aws-sdk/client-dynamodb'
+import { marshall, unmarshall } from '@aws-sdk/util-dynamodb'
+import { DynamoAttributeType } from '../../dynamo-attribute-types'
+import { type ListAttributeMetadata } from '../../metadata/attribute-types/list.metadata'
+import { AttributeType } from '../../tables/attribute-type'
 
-//     const list = []
-//     // debug('parsing list')
+type Value = any[]
+type Metadata = ListAttributeMetadata
 
-//     if (_.isArray(v)) {
-//       for (let i = 0; i < v.length; i++) {
-//         // TODO assume only one attribute type allowed for a list
-//         const attrType = attr.attributes.get(0)
+export class ListAttributeType extends AttributeType<Value, Metadata> {
+  type = DynamoAttributeType.List
 
-//         if (attrType) {
-//           const attrVal = attrType.parseDynamo(v[i])
-//           if (!_.isNil(attrVal)) {
-//             list.push(attrVal)
-//           }
-//         }
-//       }
-//     }
+  toDynamo(value: Value): AttributeValue {
+    return marshall({ value }, this.metadata?.marshallOptions).value
+  }
 
-//     return list
-//   },
-// },
+  fromDynamo(value: AttributeValue | null): Value | null {
+    return value == null
+      ? null
+      : unmarshall({ value }, this.metadata?.unmarshallOptions).value
+  }
+}
