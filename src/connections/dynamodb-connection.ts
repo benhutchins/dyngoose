1d972ba1a5f521219cb2468f4fd91fdb25d243f7
Benjamin Hutchins <ben@hutchins.co>
2023-09-14 23:45:17 -0400

    fix: restore support for AWS XRAY
    Moving the xray library to an optional dependency as it can be safely
    kept uninstalled when it is not necessary.

--- src/connections/dynamodb-connection.ts
+++ src/connections/dynamodb-connection.ts
@@ -10,15 +10,14 @@ export class DynamoDBConnection implements Connection {
   private readonly __client: DynamoDB
 
   constructor(options: DyngooseDynamoDBConnectionOptions) {
-    // if (options.enableAWSXray === true) {
-    //   // Since "require" itself does something for this lib, such as logging
-    //   // importing this only when it's needed
-    //   const AWSXRay = require('aws-xray-sdk-core')
-    //   const aws = AWSXRay.captureAWS(AWS)
-    //   this.__client = new aws.DynamoDB(options)
-    // } else {
-    this.__client = new DynamoDB(options)
-    // }
+    if (options.enableAWSXray === true) {
+      // Since "require" itself does something for this lib, such as logging
+      // importing this only when it's needed
+      const AWSXRay = require('aws-xray-sdk-core')
+      this.__client = AWSXRay.captureAWSv3Client(new DynamoDB(options))
+    } else {
+      this.__client = new DynamoDB(options)
+    }
   }
 
   public get client(): DynamoDB {
