b43832504b17d6e716f277893aed0a65267e04e0
Benjamin Hutchins <ben@hutchins.co>
2023-09-14 23:36:51 -0400

    fix: re-add support for DYNAMO_ACCESS_KEY_ID & DYNAMO_SECRET_ACCESS_KEY

--- src/config.ts
+++ src/config.ts
@@ -5,10 +5,20 @@ export default class Config {
 
   public static get defaultConnection(): Connection {
     if (this.__defaultConnection == null) {
+      let credentials: { accessKeyId: string, secretAccessKey: string } | undefined
+
+      if (typeof process.env.DYNAMO_ACCESS_KEY_ID === 'string' && typeof process.env.DYNAMO_SECRET_ACCESS_KEY === 'string') {
+        credentials = {
+          accessKeyId: process.env.DYNAMO_ACCESS_KEY_ID,
+          secretAccessKey: process.env.DYNAMO_SECRET_ACCESS_KEY,
+        }
+      }
+
       this.__defaultConnection = new DynamoDBConnection({
         endpoint: process.env.DYNAMO_ENDPOINT,
         region: process.env.DYNAMO_REGION,
         enableAWSXray: process.env.ENABLE_XRAY === 'true',
+        credentials,
       })
     }
     return this.__defaultConnection
